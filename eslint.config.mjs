adb1cfb8c304c22c0349cde847ebab897cf0abf4
a11n-dev <im.a11n.dev@gmail.com>
2026-01-29 17:40:45 +0100

    chore: unify string quotes and format ESLint, Playwright, and Vitest config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
-import withNuxt from './node_modules/.cache/nuxt/.nuxt/eslint.config.mjs'
+import withNuxt from "./node_modules/.cache/nuxt/.nuxt/eslint.config.mjs";
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt();
+// Your custom configs here
